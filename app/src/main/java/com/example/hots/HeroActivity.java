4fe5b70c7f3a0287a85f1ad79d1a91a230d63900
Lisa <lisa.kntz@gmail.com>
2019-04-01 21:16:02 +0200

    Added images
    +Added heroes images when clicked
    +Added background

--- app/src/main/java/com/example/hots/HeroActivity.java
+++ app/src/main/java/com/example/hots/HeroActivity.java
@@ -1,11 +1,14 @@
 package com.example.hots;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class HeroActivity extends AppCompatActivity {
     TextView heroNom;
@@ -15,10 +18,11 @@ public class HeroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hero_activity);
-
         heroNom = findViewById(R.id.nomHero);
+        imageView = findViewById(R.id.imageView);
         Intent intent = getIntent();
         String receivedName =  intent.getStringExtra("tvName");
+        Picasso.get().load("https://blzmedia-a.akamaihd.net/heroes/cardPortraits/alexstrasza.jpg").into(imageView);
 
         heroNom.setText(receivedName);
         //enable back Button
